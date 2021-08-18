e794c768a13307d76da985f8e9a86e296d1f9c98
Mahmud841 <mahmudsuruj14@gmail.com>
2021-08-19 06:30:57 +0800

    last commit

---
+++ try-catch.js
@@ -0,0 +1,17 @@
+ 
+/* const name = 'ami';
+name = "tumi";
+console.log(name); */
+let name = 'ami';
+name = "tumi";
+console.log(name); 
+
+try {
+    name = 'tumi';
+    name = 'hami';
+}
+catch(error){
+    console.log('getting error',error);
+
+}
+console.log(name);
