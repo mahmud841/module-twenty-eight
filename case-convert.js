0599daf19bc78f892fce5e6d7615c9481fd6d1b3
Mahmud841 <mahmudsuruj14@gmail.com>
2021-08-19 02:12:51 +0800

    second commit

--- case-convert.js
+++ case-convert.js
@@ -1,2 +1,11 @@
 const anthem = "Amr Sonar Bangla";
 const search = "valo";
+
+const userInput = 'blackPink';
+const savedUserName = 'blACKPink';
+// console.log(savedUserName.toLowerCase());
+// console.log(userInput.toLowerCase());
+// console.log(userInput.toUpperCase());
+if (userInput.toUpperCase() == savedUserName.toUpperCase()) {
+    console.log('user exists');
+}
